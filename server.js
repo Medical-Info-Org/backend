0b966c474786562c9546c55e0b9b7eb3caf13bc2
Max Kolbe <86488799+MaxKolbe@users.noreply.github.com>
2024-06-20 11:16:22 +0100

    DailyTips (#1)

--- server.js
+++ server.js
@@ -1,10 +1,22 @@
 const express = require("express")
 const connectSQLdb = require("./database/mySQLdb")
-const app = express()
-const dotenv = require("dotenv").config()
+const dailyTips = require("./Routes&Controllers/Dailytips/dailytipsController")
 
+require("dotenv").config()
+
+const app = express()
 const port = process.env.PORT
+
+app.use(express.json())//JSON middleware
+app.use('/dailyTips', dailyTips)
+
 connectSQLdb()
+
+//catch errors middleware
+app.use((err, req, res, next) => {
+    console.log(err);
+    res.status(500).json({message: 'something broke', error: err});
+});
 
 app.listen(port, ()=>{
     console.log(`App running on port ${port}`)
